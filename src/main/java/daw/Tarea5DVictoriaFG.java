515a760b9a12891d6b77bdca9e35f2fd4f4568c3
snavgar217 <snavgar217@g.educaand.es>
2024-01-15 10:07:14 +0100

    Se ha añadido a Dumbo

--- src/main/java/daw/Tarea5DVictoriaFG.java
+++ src/main/java/daw/Tarea5DVictoriaFG.java
@@ -11,6 +11,9 @@ package daw;
 public class Tarea5DVictoriaFG {
 
     public static void main(String[] args) {
-        
+        Animales animal = new Animales();
+        animal.nombre="Dumbo";
+        animal.nombreDueño="El malo de la pelicula";
+        animal.raza="Elefante";
     }
 }
