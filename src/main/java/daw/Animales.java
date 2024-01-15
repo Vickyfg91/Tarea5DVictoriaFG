96e2cc4b838f13193b906962a6fef4719e0d77e9
vfergon346@g.educaand.es <vfergon346@g.educaand.es>
2024-01-15 10:10:52 +0100

    Añadido getter y setter

--- src/main/java/daw/Animales.java
+++ src/main/java/daw/Animales.java
@@ -9,11 +9,12 @@ package daw;
  * @author vickyfg
  */
 public class Animales {
-    String nombre;
-    String raza;
-    int edad;
-    String nombreDueño;
+    private String nombre;
+    private String raza;
+    private int edad;
+    private String nombreDueño;
 
+    //Constructor parametrizado
     public Animales(String nombre, String raza, int edad, String nombreDueño) {
         this.nombre = nombre;
         this.raza = raza;
@@ -21,7 +22,42 @@ public class Animales {
         this.nombreDueño = nombreDueño;
     }
 
+    //Contructor sin parametros
     public Animales() {
+    }
+    
+    //Getter y Setter
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombreDueño() {
+        return nombreDueño;
+    }
+
+    public void setNombreDueño(String nombreDueño) {
+        this.nombreDueño = nombreDueño;
     }
     
     
